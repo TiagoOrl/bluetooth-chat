af270147cec16b18b462aced26d8bcd5d98f89f9
TiagoOrl <tiago_orlando@yahoo.com>
2023-11-23 00:28:32 -0300

    refactor: moved calback methods

--- app/src/main/java/com/asm/bluetoothchat/bluetooth/BluetoothClient.kt
+++ app/src/main/java/com/asm/bluetoothchat/bluetooth/BluetoothClient.kt
@@ -4,41 +4,33 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
-import android.os.Handler
 import com.asm.bluetoothchat.Constants
 import java.io.IOException
-import java.lang.RuntimeException
 import java.util.UUID
 
 @SuppressLint("MissingPermission")
 class BluetoothClient(
     private val bluetoothAdapter: BluetoothAdapter?,
-    private val onGetSocket: (socket: BluetoothSocket) -> Unit
+    bluetoothDevice: BluetoothDevice,
+    private val onGetSocket: (socket: BluetoothSocket?) -> Unit,
+    private val onClientConnError: (msg: String) -> Unit
 ) : Thread() {
-    private lateinit var socket: BluetoothSocket
+    private var socket: BluetoothSocket? = null
 
-    fun createClientConnection(device: BluetoothDevice) {
-        socket = device.createRfcommSocketToServiceRecord(UUID.fromString(Constants.APP_UUID))
+    init {
+        socket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString(Constants.APP_UUID))
     }
 
     override fun run() {
         // Cancel discovery because it otherwise slows down the connection.
         bluetoothAdapter!!.cancelDiscovery()
         try {
-            socket.connect()
+            socket?.connect()
         } catch (e: IOException) {
             e.printStackTrace()
-            throw e
+            onClientConnError(e.toString())
         }
         onGetSocket(socket)
     }
 
-    fun cancel() {
-        try {
-            socket.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            throw e
-        }
-    }
 }
